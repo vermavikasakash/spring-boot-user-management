afbe608c97badd4cbbb48969d2acb49f453aecd9
vermavikasakash <vikas26061995@gmail.com>
2026-09-14 10:35:15 +0530

    Validation and Exception Handling using @Valid

--- src/main/java/com/example/UserManagement/dto/CreateUserDto.java
+++ src/main/java/com/example/UserManagement/dto/CreateUserDto.java
@@ -1,7 +1,14 @@
 package com.example.UserManagement.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateUserDto {
+    @NotBlank(message = "Name is required")
     String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
     String email;
 
     public CreateUserDto(String name, String email) {
